7c0d50c61c4e43bbb93e9726a7b12c0630a26250
romanini <gustavo.romanini@beuni.com.br>
2022-10-31 17:21:50 -0300

    refactor(utils): change variables names to patternize with the rest of the code

--- server/src/utils.cpp
+++ server/src/utils.cpp
@@ -11,20 +11,20 @@
 #include "../include/entities.hpp"
 #define OK "#"
 
-Match *createMatch(int socketPlayer1, int socketPlayer2)
+Match *createMatch(int player1Socket, int player2Socket)
 {
     char message[2];
     message[1] = '\0';
 
     message[0] = PLAYER_1;
-    send(socketPlayer1, message, 2, 0);
+    send(player1Socket, message, 2, 0);
 
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
     message[0] = PLAYER_2;
-    send(socketPlayer2, message, 2, 0);
+    send(player2Socket, message, 2, 0);
 
-    Match *gameMatch = new Match(socketPlayer1, socketPlayer2);
+    Match *gameMatch = new Match(player1Socket, player2Socket);
 
     return gameMatch;
 }
